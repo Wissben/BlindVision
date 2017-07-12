f4f4ce20c0f5ec476118f33058adaec01b4c9bdc
Sidahmed <sidahmed.malaoui@gmail.com>
2017-07-12 14:46:36 +0100

    Create the class `Direction`
    This class is used in functions like `move(Direction d);` to decide where to move.

---
+++ app/src/main/java/com/example/wiss/myapplication/Direction.java
@@ -0,0 +1,13 @@
+package com.example.wiss.myapplication;
+
+/**
+ * Created by Sidahmed on 12/07/17.
+ * It is used in functions like `move(Direction d);` to decide where to move.
+ */
+
+public enum Direction {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN;
+}
