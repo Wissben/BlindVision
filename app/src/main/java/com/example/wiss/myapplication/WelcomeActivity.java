7a467d24f11f8806904d9dde0ddd3a398657a91e
Sidahmed <sidahmed.malaoui@gmail.com>
2017-07-18 00:42:49 +0100

    Create class 'SwipeChoice'
     - Create class 'SwipeChoice' that give swipe choice to the player.
     - Implement 'Choiceable' methods in 'WelcomeActivity'.

--- app/src/main/java/com/example/wiss/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/wiss/myapplication/WelcomeActivity.java
@@ -2,12 +2,44 @@ package com.example.wiss.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements Choiceable {
+    public View.OnTouchListener handleSwipe;
+    /* We put the static objects here (like gameLogic and stuff). */
 
-    @Override
+
+    /* Methods =================================================================================== */
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        /* We tell the player the available choices before using the next instructions. */
+
+        this.handleSwipe = new SwipeChoice(this);
+        this.findViewById(android.R.id.content).setOnTouchListener(this.handleSwipe);
+    }
+
+
+    @Override
+    public void up() {
+        Log.i("CHOICE", "The player choosed up");
+    }
+
+    @Override
+    public void down() {
+        Log.i("CHOICE", "The player choosed down");
+    }
+
+    @Override
+    public void left() {
+        Log.i("CHOICE", "The player choosed left");
+    }
+
+    @Override
+    public void right() {
+        Log.i("CHOICE", "The player choosed right");
     }
 }
