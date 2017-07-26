8e5e37583d32bfdfde1cef164769a4e67f058a02
wissben <wissben69@gmail.com>
2017-07-26 16:19:55 +0100

    Adding RunawayGameLogic

---
+++ app/src/main/java/com/example/wiss/game/RunawayGameLogic.java
@@ -0,0 +1,21 @@
+package com.example.wiss.game;
+
+import com.example.wiss.units.SoundSource;
+
+import java.util.LinkedList;
+
+/**
+ * Created by wiss on 24/07/17.
+ */
+
+public class RunawayGameLogic extends GameLogic {
+
+    public RunawayGameLogic(LinkedList<SoundSource> soundSources) {
+        super(soundSources);
+    }
+
+    @Override
+    public void movePlayerToPos(double x, double y) {
+
+    }
+}
